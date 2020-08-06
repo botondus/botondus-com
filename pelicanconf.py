c85431802b7a91321903e719cd97f17828cea861
Botond Beres <botondus@gmail.com>
2020-08-06 18:34:04 +0300

    Configured URLs, archives and Author page.

--- pelicanconf.py
+++ pelicanconf.py
@@ -48,3 +48,18 @@ TYPOGRIFY = True
 # DO NOT automatically generate slugs
 # For how to write slugs see https://www.theengineeringprojects.com/2018/07/structure-blog-post-url.html
 SLUGIFY_SOURCE = ''
+
+# Configure URLs
+#ARTICLE_URL = '{slug}.html'
+ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/index.html'
+#PAGE_URL = 'pages/{slug}.html'
+PAGE_SAVE_AS = 'pages/{slug}/index.html'
+
+YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/index.html'
+
+# If you do not want one or more of the default pages to be created
+# (e.g., you are the only author on your site and thus do not need an Authors page),
+# set the corresponding *_SAVE_AS setting to '' to prevent the relevant page
+# from being generated.
+AUTHOR_SAVE_AS = ''
