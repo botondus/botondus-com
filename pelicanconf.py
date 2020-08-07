1d2477b4bcbda00d0b1e4fc932a2da90f09ac8fc
Botond Beres <botondus@gmail.com>
2020-08-07 18:29:51 +0300

    Installed the Blue Penguin theme.
    See https://github.com/jody-frankowski/blue-penguin

--- pelicanconf.py
+++ pelicanconf.py
@@ -86,3 +86,22 @@ PLUGINS = [
 ERT_WPM = 200
 ERT_FORMAT = '{time} read'
 ERT_INT = True
+
+THEME = "/Users/beresbotond/Projects/pelican-themes/blue-penguin"
+
+TAGS_URL = 'tags'
+TAGS_SAVE_AS = 'tags/index.html'
+AUTHORS_URL = 'authors'
+AUTHORS_SAVE_AS = 'authors/index.html'
+CATEGORIES_URL = 'categories'
+CATEGORIES_SAVE_AS = 'categories/index.html'
+ARCHIVES_URL = 'archives'
+ARCHIVES_SAVE_AS = 'archives/index.html'
+
+# use those if you want pelican standard pages to appear in your menu
+MENU_INTERNAL_PAGES = (
+    #('Tags', TAGS_URL, TAGS_SAVE_AS),
+    #('Authors', AUTHORS_URL, AUTHORS_SAVE_AS),
+    ('Categories', CATEGORIES_URL, CATEGORIES_SAVE_AS),
+    ('Archives', ARCHIVES_URL, ARCHIVES_SAVE_AS),
+)
